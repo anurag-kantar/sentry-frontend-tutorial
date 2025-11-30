26e8590514bf9a28ebaaa94383bb6f64c8591805
anuragp100 <anurag.pandey1@kantar.com>
2025-11-30 19:00:53 +0530

    feat: simplify app to hello world

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-import ReactDOM from 'react-dom/client';
-import App from "./components/App";
+import ReactDOM from "react-dom/client";
 
-const container = document.getElementById('root');
-const root = ReactDOM.createRoot(container);
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<h1>Hello World</h1>);
